8c669bf7aea7d92f13e35560d30e6ac2e4fd12ef
lovechuck <125598016@qq.com>
2018-11-01 15:30:36 +0800

    --base

--- 02/main.go
+++ 02/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+/*
+测试bool
+*/
+func testBool() {
+	var isLast = false
+	fmt.Println(isLast)
+}
+
+/*
+测试数值
+*/
+func testNumber() {
+	var a int32 = 2
+	var b float32 = 4.67890
+	var c complex64 = 5 + 5i
+	fmt.Println(a, b, c)
+}
+
+/*
+测试字符串
+*/
+func testSring() {
+	var a = `hello`
+	var b = ` world`
+	fmt.Println(a + b)
+}
+
+/*
+测试错误
+*/
+func testError() {
+	err := errors.New("fatal error")
+	fmt.Println(err)
+}
 
 func main() {
-	fmt.Printf("hello world\n")
+	var name, age = "chuck", 12
+	fmt.Println(name)
+	fmt.Println(strconv.Itoa(age))
+
+	testBool()
+	testNumber()
+	testSring()
+	testError()
 }
